7dd6f5ef23dceee11df4f1d5111a2cc5a72ca9ce
Ryan Lewis <ryan@ryztech.co>
2021-01-12 01:07:45 -0500

    First push

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = commands.Bot(command_prefix = '.')
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_voice_state_update(member, prev, cur):
+    user = f"{member.name}#{member.discriminator}"
+    vc = member.guild.voice_client
+    if cur.self_mute and not prev.self_mute: # Would work in a push to talk channel
+        print(f"{user} stopped talking!")
+        if not vc.is_playing():
+            vc.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source="mic_click_on.wav"))
+            vc.source = discord.PCMVolumeTransformer(vc.source)
+            vc.source.volume = 0.5
+    elif prev.self_mute and not cur.self_mute: # As would this one
+        print(f"{user} started talking!")
+        if not vc.is_playing():
+            vc.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source="mic_click_off.wav"))
+            vc.source = discord.PCMVolumeTransformer(vc.source)
+            vc.source.volume = 0.5
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    if ctx.author.voice and ctx.author.voice.channel:
+        channel = ctx.author.voice.channel
+    else:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    await channel.connect()
+
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconect()
+
+client.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Discord Radio Effects Bot
+
+This is simple script i made in python to add effects to discord channels for RP servers. Its not perfect you are more than welcome to contribute.
+
+### Usage
+
+`.join` - Joins the bot to the server
+`.leave` - Makes the bot leave
+- Setup hot keys to mute and unmute to activate sound effects
+- Disable default Discord sound effects
+
+
+### Features
+
+- Customizable radio effects on mute and unmute
+- join and un-join commands
+
+### Requirements
+Python 3
+PIP
+discord.py
+discord.py[voice]
+python-dotenv
+ffmpeg.exe [LINK](https://ffmpeg.org/download.html#build-windows "LINK")
+Radio Effects of your choice [(Link to the ones i used)](https://www.lcpdfr.com/downloads/gta5mods/audio/10897-motorola-mdc1200-mic-clicks/# "(Link to the ones i used)")
+
+### Installation 
+`pip install -U discord.py`
+`pip install -U discord.py[voice]`
+`pip install -U python-dotenv`
+
+place FFMPEG.exe and the audio files into the root folder (if using different audio files update the script with correct names)
+
+###Discord Integration
+- Go to the developer portal and create a new applicaiton
+https://discord.com/developers/applications
+
+- Go to bots and add bot
+
+- Create a username and click copy token
+
+- Create .env file and add `DISCORD_TOKEN="YOURTOKEN"`
+
+- Replace YOURTOKEN with token from dev portal and save .env file
+
+- Go to OAuth2, select BOT scope and copy the link.
+
+- Paste the link into your browser and invite the bot to your server.
+
+###Starting the bot
+`phython main.py`
+
+
+###End
